e10cc3fdbfaf30832290dba5845b5518d7ea11a4
Nas <nassersanou23@gmail.com>
2019-01-08 03:49:35 -0500

    Ap WIP

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-
-const weather = new Weather('New York','usa', 'fr')
+// Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+// Init weather object
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+// Init UI
+const ui = new UI();
 
 // Get weather on DOM load
-document.addEventListener('DOMContentLoaded', getTheWeather);
+document.addEventListener('DOMContentLoaded', getWeather);
 
-weather.changeLocation('Florida', 'us', 'eng');
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
 
-function getTheWeather(){
+  // Change location
+  weather.changeLocation(city, state);
+
+  // Set location in LS
+  storage.setLocationData(city, state);
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+});
+
+function getWeather(){
   weather.getWeather()
     .then(results => {
-      console.log(results);
+      ui.paint(results);
     })
     .catch(err => console.log(err));
 }
